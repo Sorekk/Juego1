759e99c57eb698e0e1bdc843e17bcb1c65ce7b8e
Marc Fuente <marc.fuente@fhios.es>
2020-11-14 16:40:55 +0100

    mundo dos

---
+++ Scripts/Mundo2.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+const monedaNode = preload("res://Scenes/Moneda.tscn")
+const rect_size_x = 1024
+const rect_size_y = 600
+
+onready var jugador1 = $jugador1
+onready var jugador2 = $jugador2
+onready var numeros1 = $Numeros1.animationPlayer
+onready var numeros2 = $Numeros2.animationPlayer
+onready var mundo = $Mundo
+onready var texto = $RichTextLabel
+
+var new_moneda: = monedaNode.instance()
+#var ganador = "Ventaja de Jugador{str}"
+
+func _ready():
+	#var winner = ganador.format({"str": mundo.ganador})
+	#print(winner)
+	
+	new_moneda.global_position = _get_random_spawn_position()
+	add_child(new_moneda)
+
+func _process(delta):
+	var toca = new_moneda.get('toca')
+	if(toca):
+		new_moneda.set('toca', false)
+		new_moneda.global_position = _get_random_spawn_position()
+		add_child(new_moneda)
+
+	numeros1.play('Contar')
+	numeros1.seek(jugador1.contadorMonedas, true)
+	numeros2.play('Contar')
+	numeros2.seek(jugador2.contadorMonedas, true)
+
+	if(jugador1.contadorMonedas == 10):
+		jugador1.contadorWins += 1
+		get_tree().change_scene("res://Scenes/Mundo3.tscn")
+	elif(jugador2.contadorMonedas == 10):
+		jugador2.contadorWins += 1
+		get_tree().change_scene("res://Scenes/Mundo3.tscn")
+
+func _get_random_spawn_position() -> Vector2:
+	var spawn_pos: = Vector2(
+	rand_range(0, rect_size_x),
+	rand_range(0, rect_size_y))
+	return spawn_pos
